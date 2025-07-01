d57fd0c19bee9c6b0aef1abc3ec3929dbe5998c2
Yulric Sequeira <yulrics@gmail.com>
2025-07-01 17:56:21 -0400

    test

--- R/post_render_script.R
+++ R/post_render_script.R
@@ -3,6 +3,9 @@
 # This post-render script demonstrates the issue with renv libraries
 # not being found during quarto render
 
+setwd('../')
+source('.Rprofile')
+
 cat("Post-render script starting...\n")
 
 # Try to load renv-managed packages
